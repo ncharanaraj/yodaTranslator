53342bd214e4653c61de1870cb7e41e50278f72e
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-23 20:25:36 +0530

    initial commit

---
+++ yoda.js
@@ -0,0 +1,3 @@
+var inputText = document.querySelector("#input-translator")
+var btnTranslate = document.querySelector("#translation-btn")
+var outputMessage = document.querySelector("#output-translator")
