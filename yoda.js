8bd49a49daf5def204afebbb376d4c18b9332f5b
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-24 12:35:19 +0530

    input output processing

--- yoda.js
+++ yoda.js
@@ -1,3 +1,21 @@
 var inputText = document.querySelector("#input-translator")
 var btnTranslate = document.querySelector("#translation-btn")
 var outputMessage = document.querySelector("#output-translator")
+
+var yodaAPI = "https://api.funtranslations.com/translate/yoda.json"
+
+function getUrlTranslation(text) {
+   return yodaAPI + "?" + "text=" + text;
+}
+
+function clickEventHandler() {
+    var userInput = inputText.value;
+
+    fetch(getUrlTranslation(userInput))
+    .then(Response => Response.json()
+    .then(json => {
+        outputMessage.innerText = json.contents.translated;
+    }))
+}
+
+btnTranslate.addEventListener('click', clickEventHandler);
